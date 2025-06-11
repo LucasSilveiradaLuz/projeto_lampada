b8ca6a9644576fa8616e2425b038aa23826c9559
LucasSilveiradaLuz <luzlucas22022007@gmail.com>
2025-06-11 11:45:13 -0300

    Enviando os arquivos do Projeto lampada finalizados

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+ let image = document.getElementById("lampada")
+let button= document.getElementById("button")
+
+let index = false
+
+
 function lampada(){
-    let aceso = false
-    if(){
-        
-    }
+   
+
+  if(!index){
+      image.src = "lampada apagada.png";
+       button.innerText = "Acender"
+       index = true
+  }
+  else{
+      image.src = "lampada acesa.png";
+        button.innerText = "Apagar"
+        index = false 
+  }
+
 }
