a562c8cca63786e91c7d2bb5cdec4e3d4f87d888
LucasSilveiradaLuz <luzlucas22022007@gmail.com>
2025-06-13 09:28:07 -0300

    Enviando os arquivos do projeto lampada finalizados

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
- let image = document.getElementById("lampada")
-let button= document.getElementById("button")
+ let image = document.getElementById("lampada")//pega o id da imagem
 
-let index = false
+let button= document.getElementById("button")//pega o id do botão 
+
+let index = false //coloca inicialmente como falso 
+//tudo fora da função para executar primeiro,a captura de informações
 
 
 function lampada(){
    
 
-  if(!index){
+  if(!index){//se index for diferente mostrar a imagem lampada apagada
       image.src = "lampada apagada.png";
-       button.innerText = "Acender"
-       index = true
+       button.innerText = "Acender" //nome do botão,que depois irá mudar
+       index = true //colocar index como true
   }
-  else{
+  else{//senão mostrar a imagem da lampada acessa
       image.src = "lampada acesa.png";
-        button.innerText = "Apagar"
-        index = false 
+        button.innerText = "Apagar" //botão será reescrito como Apagar
+        index = false //coloca-se index = false para o código se repetir 
   }
 
 }
